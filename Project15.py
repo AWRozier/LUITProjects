fe4e23506f39104ac242e9d61847d61176492163
AWRozier <alexuswoods@gmail.com>
2023-01-16 01:13:58 +0000

    Latest Project

---
+++ Project15.py
@@ -0,0 +1,15 @@
+"""
+Project 15
+"""
+#Create a Standard SQS Queue using Python
+
+import boto3
+
+# Obtain the service resource
+sqs = boto3.resource('sqs')
+
+# Queue will be created & output will returns an SQS Queue instance
+queue = sqs.create_queue(QueueName='1stQueue')
+
+print(queue.url)
+
